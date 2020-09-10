f945ff607e88b7cc682947084c942be6d495fd40
Esraa Jbara <jbesraa@gmail.com>
2020-09-10 13:25:00 +0300

    add exp tab

--- src/App.js
+++ src/App.js
@@ -3,38 +3,39 @@ import { ReactComponent as GithubLogo } from "./github-logo.svg";
 import { ReactComponent as LinkedInLogo } from "./linkedin-logo.svg";
 
 const NavBar = () => {
-  return (
-    <>
-      <div>
-        <GithubLogo />
-      </div>
-      <div>
-        <LinkedInLogo />
-      </div>
-    </>
-  );
+	return (
+		<div className="navBarWrapper">
+			<div className="center">
+       Experience 
+			</div>
+			<div className="end">
+				<GithubLogo />
+				<LinkedInLogo />
+			</div>
+		</div>
+	);
+};
+
+const AboutMe = () => {
+	return (
+		<>
+			<div>Hello,</div>
+			<div>
+          Im Esraa, Software Developer based in Tel Aviv.
+			</div>
+		</>
+	);
 };
 
 const App = () => {
-  return (
-    <div>
-      <div className="navBarWrapper">
-        <NavBar />
-      </div>
-      <div className="container">
-        <div>Hello</div>
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<NavBar />
+			<div className="container">
+				<AboutMe/>
+			</div>
+		</div>
+	);
 };
 
 export default App;
